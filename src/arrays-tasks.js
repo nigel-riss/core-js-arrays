e24fd14e5de7fea0cf614c3c2a74c6933ccab74f
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2024-12-07 05:20:27 +0200

    feat: add calculateBalance solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -313,8 +313,10 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((prev, curr) => {
+    return prev + curr[0] - curr[1];
+  }, 0);
 }
 
 /**
