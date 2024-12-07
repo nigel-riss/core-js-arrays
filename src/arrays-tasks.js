921616344a62023502a09b943ee55e253200a406
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2024-12-07 03:26:29 +0200

    feat: add getAverage solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -123,8 +123,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const average = arr.reduce((acc, curr) => acc + curr, 0) / (arr.length || 1);
+
+  return +average.toFixed(2);
 }
 
 /**
